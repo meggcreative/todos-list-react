ff60a324991d181e5c27ac141bba73e282e56e96
Magdalena Zulewska <madziapernach@gmail.com>
2021-06-14 17:43:46 +0200

    adding empty task blocked

--- src/Form/index.js
+++ src/Form/index.js
@@ -7,6 +7,9 @@ const Form = ({addNewTask}) => {
 
     const onFormSubmit = (event) => {
         event.preventDefault();
+        if( newTaskContent.trim() === "") {
+            return;
+        }
 
         addNewTask(newTaskContent.trim());
         setNewTaskContent("");
