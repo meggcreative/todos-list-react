1ae2d6c5c4f6ff2d2db7e520cb40382642524f83
Magdalena Zulewska <madziapernach@gmail.com>
2021-06-28 21:11:47 +0200

    changed disabled for button "ukończ wszystkie"

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -1,28 +1,27 @@
 import "./style.css";
 
-const Buttons = ({tasks, hideDone, toggleHideDone, setAllDone}) => {
-   if(tasks.length === 0) {
-       return null
-   } 
+const Buttons = ({ tasks, hideDone, toggleHideDone, setAllDone }) => {
+    if (tasks.length === 0) {
+        return null
+    }
     return (
         <div className="buttons">
-            <button 
+            <button
                 onClick={toggleHideDone}
                 className="buttons__button"
-                disabled={tasks.every(({done})=> done)}
             >
-                {hideDone ? "Pokaz" : "Ukryj"} ukończone 
+                {hideDone ? "Pokaz" : "Ukryj"} ukończone
             </button>
-            <button 
+            <button
                 onClick={setAllDone}
                 className="buttons__button"
-                disabled={tasks.every(({done}) => done)}
+                disabled={tasks.every(({ done }) => done)}
             >
                 Ukończ wszystkie
             </button>
         </div>
     )
 }
-    
+
 
 export default Buttons;
