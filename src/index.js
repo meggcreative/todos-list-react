9c3bf096dc2c1a34a1d34a4a33bf0143ae220c9c
Magdalena Zulewska <madziapernach@gmail.com>
2021-07-24 14:12:24 +0200

    aplication split into features

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ThemeProvider } from "styled-components";
 import './index.css';
-import App from './App';
+import App from "./App.js" ;
 import reportWebVitals from './reportWebVitals';
 import {theme} from "./theme.js";
 
